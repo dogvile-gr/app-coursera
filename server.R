c6bd1a991e8252a87869f0d24680607b1c4d37b1
dogvile-gr <info@dogvile.com>
2015-02-22 12:14:49 +0200

    last

--- server.R
+++ server.R
@@ -1,11 +1,28 @@
 
-bmi <- function(height,weight){(weight*0.45455/(0.0254*height)^2)}
+bmi <- function(height,weight){
+        
+        bmi <- ((weight)/(height)^2)
+        return (bmi)
+
+}
+
+category <- function(bmi) {
+        if (bmi < 18.5) "underweight"
+        else if (bmi > 25) "overweight"
+        else "normal"
+}
+
+
+
 
 shinyServer(
         function(input, output) {
-
-                output$inputValue <- renderPrint({input$height;input$weight})
+               
+                
+                output$inputValue1 <- renderPrint({input$height})
+                output$inputValue2 <- renderPrint({input$weight})
                 output$bmi <- renderPrint({bmi(input$height,input$weight)})
+                category <- renderPrint({category(output$bmi)})
         }
 )
 
--- ui.R
+++ ui.R
@@ -3,25 +3,34 @@ shinyUI(
                 # Application title
                 headerPanel("Body mass Index"),
                 sidebarPanel(
-
-                        numericInput('height', 'cm', 160, min = 148, max = 210, step = 0.1),
+                        numericInput('height', 'm', 1.5, min = 1.5, max = 2.1, step = 0.01),
                         numericInput('weight', 'kgr', 40, min = 100, max = 200, step = 0.1),
-                        submitButton('Submit'),
-                        conditionalPanel(
-                                condition = "output$bmi <= '1'",
-                                p("Conditional text here")
-                                )
-                ),
+                        submitButton('Submit')
+                       ),
                 mainPanel(
 
-                        h3('Results of prediction'),
-                        h4('You entered'),
-                        verbatimTextOutput("inputValue"),
-                        verbatimTextOutput("inputValue"),
-                        h4('Which resulted in a prediction of '),
+                        h4('Your height is: '),                  
+                        verbatimTextOutput("inputValue1"),
+                        h4('Your weight is: '),
+                        verbatimTextOutput("inputValue2"),
+               
                        
-                        h4('bmi'),
-                        verbatimTextOutput("bmi")
+                        h4('Your Body Mass index is: '),
+                        verbatimTextOutput("bmi"),
+#                         h4('You belong in category: '),
+#                         verbatimTextOutput("category"),
+                        br(),
+                        
+                        
+                        
+                        
+p(style = "font-size:11px","BMI is used differently for children. It is calculated in the same way as for adults, but then compared to typical values for other children of the same age. Instead of comparison against fixed thresholds for underweight and overweight, the BMI is compared against the percentile for children of the same gender and age"),
+br(),
+p(style = "font-size:11px","A BMI that is less than the 5th percentile is considered underweight and above the 95th percentile is considered obese. Children with a BMI between the 85th and 95th percentile are considered to be overweight."),
+br(),
+p(style = "font-size:11px","Recent studies in Britain have indicated that females between the ages 12 and 16 have a higher BMI than males of the same age by 1.0 kg/m2 on average"),
+br(),
+p(style = "font-size:8px","source: http://en.wikipedia.org/wiki/Body_mass_index")
 
                 )
 
